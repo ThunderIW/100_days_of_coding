5da0591e1df517fe3569e67d1c495297c400e8e9
ThunderIW <43197270+ThunderIW@users.noreply.github.com>
2024-05-02 03:21:59 -0700

    added two new lines of code How_many_weeks_left.py so that you can input the expected age range of your country

--- Day_2/How_many_weeks_left.py
+++ Day_2/How_many_weeks_left.py
@@ -1,6 +1,6 @@
-age=int(input())
-
-Remaining_life_span=81-age
+age=int(input("How old are you ?"))
+expected_life_range=82
+Remaining_life_span=expected_life_range-age
 
 weeks_remaining=(Remaining_life_span*52)
 print(weeks_remaining)
